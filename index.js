3036a31234e8296d6b6f218064e69675d315b130
unknown <javijimenezclemente@gmail.com>
2022-08-10 21:45:07 +0200

    Improved code, added event listeners instead of html onclick and iterators

--- index.js
+++ index.js
@@ -1,73 +1,82 @@
-let board = [];
-board[0] = false
-board[1] = document.getElementById("1")
-board[2] = document.getElementById("2")
-board[3] = document.getElementById("3")
-board[4] = document.getElementById("4")
-board[5] = document.getElementById("5")
-board[6] = document.getElementById("6")
-board[7] = document.getElementById("7")
-board[8] = document.getElementById("8")
-board[9] = document.getElementById("9")
+const board = Array.from(document.querySelectorAll('.square'))
+board.unshift(false)
+const resetButton = document.querySelector(".reset-button")
+
 let popUp = document.getElementById("popup")
 let winnerName = document.getElementById("winner-name")
 let someoneWon = false;
-function didIWin(player){
-    if (
-        (board[1].innerHTML == player && board[2].innerHTML == player && board[3].innerHTML == player) ||
-        (board[4].innerHTML == player && board[5].innerHTML == player && board[6].innerHTML == player) ||
-        (board[7].innerHTML == player && board[8].innerHTML == player && board[9].innerHTML == player) ||
-        (board[1].innerHTML == player && board[4].innerHTML == player && board[7].innerHTML == player) ||
-        (board[2].innerHTML == player && board[5].innerHTML == player && board[8].innerHTML == player) ||
-        (board[3].innerHTML == player && board[6].innerHTML == player && board[9].innerHTML == player) ||
-        (board[1].innerHTML == player && board[5].innerHTML == player && board[9].innerHTML == player) ||
-        (board[7].innerHTML == player && board[5].innerHTML == player && board[3].innerHTML == player)
-        ){
-        someoneWon = true;
-        winnerName.innerHTML = player + " won!!"
-        popUp.style.display = "block"
-    }
+
+const winningConditions = [
+    [1, 2, 3], 
+    [4, 5, 6], 
+    [7, 8, 9], 
+    [1, 4, 7], 
+    [2, 5, 8], 
+    [3, 6, 9], 
+    [1, 5, 9], 
+    [7, 5, 3]
+]
+
+function didIWin(player) {
+    winningConditions.forEach((value, index) => {
+        const a = board[value[0]].innerHTML
+        const b = board[value[1]].innerHTML
+        const c = board[value[2]].innerHTML
+        if ((a != "" && b != "" && c != "") && (a == b && b == c)) {
+            someoneWon = true;
+            winnerName.innerHTML = player + " won!!"
+            popUp.style.display = "block"
+        }
+
+    })
 }
+
 function isItOver() {
     if (someoneWon == false) {
-        
-    
-    if (board[1].innerHTML && board[2].innerHTML && board[3].innerHTML && board[4].innerHTML && board[5].innerHTML && board[6].innerHTML && board[7].innerHTML && board[8].innerHTML && board[9].innerHTML) {
-        someoneWon = true
-                winnerName.innerHTML = "Game over :("
-        popUp.style.display = "block"
+        let boardCheck = []
+        board.forEach((value, index) => {
+            boardCheck.push(value.innerHTML)
+        })
+        if (!boardCheck.includes("")) {
+            someoneWon = true
+            winnerName.innerHTML = "It's a tie!"
+            popUp.style.display = "block"
         }
     }
 }
-function clickOnSquare(event) {
+
+function clickOnSquare(index) {
     if (!someoneWon) {
-        if(!board[event.srcElement.id].innerHTML){
-           if (board[0] == false) {
-        board[event.srcElement.id].innerHTML = "X"
-        didIWin("X")
-        isItOver()
-        board[0] = true
-    } else if (board[0] == true) {
-        board[event.srcElement.id].innerHTML = "O"
-        didIWin("O")
-        isItOver()
-        board[0] = false
-    } }
+        if (!board[index].innerHTML) {
+            if (board[0] == false) {
+                board[index].innerHTML = "X"
+                didIWin("X")
+                isItOver()
+                board[0] = true
+            } else if (board[0] == true) {
+                board[index].innerHTML = "O"
+                didIWin("O")
+                isItOver()
+                board[0] = false
+            }
+        }
     }
-
 }
-function resetGame(){
+
+function resetGame() {
     board[0] = false
-    board[1].innerHTML = ""
-    board[2].innerHTML = ""
-    board[3].innerHTML = ""
-    board[4].innerHTML = ""
-    board[5].innerHTML = ""
-    board[6].innerHTML = ""
-    board[7].innerHTML = ""
-    board[8].innerHTML = ""
-    board[9].innerHTML = ""
+    board.forEach((value, index) => {
+        if (index != 0) {
+            value.innerHTML = "";
+        }
+    })
     someoneWon = false
-            popUp.style.display = "none"
+    popUp.style.display = "none"
 
 }
+board.forEach((square,index) => {
+    if (index != 0) {
+    square.addEventListener('click', () => clickOnSquare(index))        
+    }
+})
+resetButton.addEventListener('click', () => resetGame())
